7abe15b902c18477b158e49ff1739ea9de0e902e
Tibi - LAPTOP <kovacstibor42069@gmail.com>
2021-08-31 18:25:13 +0300

    Bottomm navigation bar updated

--- lib/bloc/main/main_screen.dart
+++ lib/bloc/main/main_screen.dart
@@ -1,8 +1,9 @@
-import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:my_instrument/shared/translation/app_localizations.dart';
+import 'package:line_icons/line_icons.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -39,34 +40,57 @@ class _MainPageState extends State<MainPage> {
       body: SafeArea(
         child: RouterOutlet(),
       ),
-      bottomNavigationBar:
-        CustomNavigationBar(
-          iconSize: 20.0,
-          selectedColor: Theme.of(context).colorScheme.onSurface,
-          strokeColor: Theme.of(context).colorScheme.onSurface,
-          unSelectedColor: Colors.grey[600],
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          currentIndex: _selectedPageIndex,
-          onTap: _changePageIndex,
-          items: <CustomNavigationBarItem>[
-            CustomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.home),
-            ),
-            CustomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.heart),
-            ),
-            CustomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.plus),
-            ),
-            CustomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.comments),
-            ),
-            CustomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.user),
-            ),
-          ],
-        )
-
+      bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                color: Colors.black.withOpacity(.1),
+              )
+            ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+              child: GNav(
+                rippleColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+                hoverColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+                gap: 6,
+                activeColor: Theme.of(context).colorScheme.onSurface,
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+                color: Theme.of(context).colorScheme.onSurface.withOpacity(0.8),
+                tabs: [
+                  GButton(
+                    icon: LineIcons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: LineIcons.heart,
+                    text: 'Favorites',
+                  ),
+                  GButton(
+                      icon: LineIcons.plus,
+                      text: 'Create'
+                  ),
+                  GButton(
+                    icon: LineIcons.commentsAlt,
+                    text: 'Messages',
+                  ),
+                  GButton(
+                    icon: LineIcons.user,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedPageIndex,
+                onTabChange: _changePageIndex
+              ),
+            )
+          )
+      )
     );
   }
 
